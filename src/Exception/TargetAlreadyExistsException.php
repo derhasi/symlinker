cb4ad7f11151219e48642eafc7ffea94ea680d20
Johannes Haseitl <johannes@undpaul.de>
2015-04-25 15:15:00 +0200

    Fix message

--- src/Exception/TargetAlreadyExistsException.php
+++ src/Exception/TargetAlreadyExistsException.php
@@ -6,6 +6,6 @@ class TargetAlreadyExistsException extends \RuntimeException {
 
     public function __construct($target, $source)
     {
-        parent::__construct(sprintf('Target "%s" is already set, but no symlink. (source "%s"', $target, $source));
+        parent::__construct(sprintf('Target "%s" is already set, but no symlink. (source "%s")', $target, $source));
     }
 }
